b937cb96e2d80f95083fb6c04bc9bb8c3c0e5576
jypark <toboy93@gmail.com>
2024-12-17 09:37:16 +0900

    에러 메시지 포맷팅

--- src/main/java/com/anycommerce/exception/GlobalExceptionHandler.java
+++ src/main/java/com/anycommerce/exception/GlobalExceptionHandler.java
@@ -4,11 +4,9 @@ import com.anycommerce.model.dto.CommonResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.stream.Collectors;
 
 @RestControllerAdvice
 @Slf4j
@@ -26,18 +24,28 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(CustomBusinessException.class)
     public ResponseEntity<CommonResponse<?>> handleCustomBusinessException(CustomBusinessException ex) {
 
-        String message = ex.getMessage();
+        // 에러 메시지 포맷팅
+        String formattedMessage = String.format(
+                "Error occurred at [%s.%s:%d] - %s",
+                ex.getStackTrace()[0].getClassName(),  // 클래스 이름
+                ex.getStackTrace()[0].getMethodName(), // 메서드 이름
+                ex.getStackTrace()[0].getLineNumber(), // 라인 번호
+                ex.getMessage()                        // 에러 메시지
+        );
 
-        if(ex.getException() != null){
-            ex.printStackTrace();
-        }
-
-        if(ex.getConsumer() != null) {
-            ex.getConsumer().accept(message);
+        // 로그 출력
+        if (ex.getException() != null) {
+            log.error(formattedMessage, ex.getException()); // 상세 에러 로그
         } else {
-            log.error(message);
+            log.error(formattedMessage); // 기본 에러 로그
         }
 
+        // Consumer 로 메시지 전달
+        if (ex.getConsumer() != null) {
+            ex.getConsumer().accept(formattedMessage);
+        }
+
+        // Response 반환
         return ResponseEntity
                 .status(ex.getErrorCode().getHttpStatus())
                 .body(CommonResponse.fromError(ex.getErrorCode(), null));
