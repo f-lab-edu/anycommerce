22ee1418b0c359561e0341697330780aec05599b
jypark <toboy93@gmail.com>
2025-02-07 14:45:35 +0900

    ci-cd 테스트용, 안쓰는 라이브러리 삭제

--- src/main/java/com/anycommerce/model/entity/Category.java
+++ src/main/java/com/anycommerce/model/entity/Category.java
@@ -1,7 +1,6 @@
 package com.anycommerce.model.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Getter;
