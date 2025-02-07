361c1807a0f3b5bc9e15466f042be222a853cebc
jypark <toboy93@gmail.com>
2025-02-07 10:28:46 +0900

    ci-cd / Dockerfile, 컴포즈 작성 완료

--- src/main/java/com/anycommerce/config/EncryptionUtil.java
+++ src/main/java/com/anycommerce/config/EncryptionUtil.java
@@ -15,7 +15,7 @@ public class EncryptionUtil{
     private final SecretKey secretKey;
 
     // @Value로 application.properties의 encryption.key 값을 주입
-    public EncryptionUtil(@Value("${encryption.key}") String encryptionKey) {
+    public EncryptionUtil(@Value("${encryption_key}") String encryptionKey) {
         if (encryptionKey.length() != 16 && encryptionKey.length() != 24 && encryptionKey.length() != 32) {
             throw new IllegalArgumentException("Invalid AES key length: " + encryptionKey.length() + " bytes. Supported lengths are 16, 24, or 32 bytes.");
         }
